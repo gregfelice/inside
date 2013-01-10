7b488940ab9e4ceb17c9482a1672afa4341049d3
Greg Felice <gregfelice@gmail.com>
2013-01-10 18:04:24 -0500

    simple_form, bootstrap, sass, screens for employees

--- flow/app/models/reporting_relationship.rb
+++ flow/app/models/reporting_relationship.rb
@@ -1,10 +1,13 @@
 class ReportingRelationship < ActiveRecord::Base
 
-  attr_accessible :dotted, :employee_id, :supervisor_id, :supervisor, :employee
+  attr_accessible :dotted, :employee_id, :supervisor_id, :supervisor, :employee_attributes, :supervisor_attributes
 
   belongs_to :employee
   belongs_to :supervisor, :class_name => "Employee"
 
   validates_uniqueness_of :employee_id, :scope => [:supervisor_id]
+
+  accepts_nested_attributes_for :employee
+  accepts_nested_attributes_for :supervisor
 
 end
--- flow/test/model/employee_test.rb
+++ flow/test/model/employee_test.rb
@@ -6,4 +6,15 @@ describe Employee do
 
   it "has a job title that is not nil"
 
+  it "has employees" do
+
+    employee = Employee.create!(full_name: "Hello World", job_title: "This is a test")
+    
+    1.must_equal 2
+
+  end
+
+
+  
+
 end
