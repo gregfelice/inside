50e9ef1f2f7103f0af78f310da4d7aed434d1a55
Greg Felice <gregfelice@gmail.com>
2013-01-10 18:04:24 -0500

    simple_form, bootstrap, sass, screens for employees

--- flow/app/models/employee.rb
+++ flow/app/models/employee.rb
@@ -1,6 +1,6 @@
 class Employee < ActiveRecord::Base
 
-  attr_accessible :contractor, :full_name, :job_title, :part_time_status
+  attr_accessible :contractor, :full_name, :job_title, :part_time_status, :reporting_relationship_attributes
   
   has_many :reporting_relationships
   has_many :employees, :through => :reporting_relationships, :uniq => true
@@ -8,5 +8,7 @@ class Employee < ActiveRecord::Base
   validates :full_name, :job_title, :presence => true
 
   validates :full_name, :uniqueness => true
+
+  accepts_nested_attributes_for :reporting_relationships
 
 end
