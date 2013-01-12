77df1e23223a241c4c555e2011463ea2b537145e
Greg Felice <gregfelice@gmail.com>
2013-01-12 17:38:10 -0500

    removed source keyword from hasmany relationship in employee - not needed

--- flow/app/models/employee.rb
+++ flow/app/models/employee.rb
@@ -3,12 +3,13 @@ class Employee < ActiveRecord::Base
   attr_accessible :full_name, :job_title, :contractor, :part_time_status, :supervisor_relationships, :subordinate_relationships
 
   validates_presence_of :full_name, :job_title
-  validates :full_name, :uniqueness => true
+  validates_uniqueness_of :full_name
 
   has_many :supervisor_relationships,     :class_name => "ReportingRelationship",      :foreign_key => :subordinate_id
   has_many :subordinate_relationships,    :class_name => "ReportingRelationship",      :foreign_key => :supervisor_id
   
-  has_many :supervisors,                  :source => :supervisor,                 :through => :supervisor_relationships
-  has_many :subordinates,                 :source => :subordinate,                :through => :subordinate_relationships
+  # - method name -    
+  has_many :supervisors,                  :through => :supervisor_relationships
+  has_many :subordinates,                 :through => :subordinate_relationships
 
 end
