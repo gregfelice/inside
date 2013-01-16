87f64f24f0a5a35da4298b4c4fb780fc6801d9c4
Greg Felice <gregfelice@gmail.com>
2013-01-16 10:25:22 -0500

    got basic add reports working, still need feature specs written

--- flow/app/controllers/employees_controller.rb
+++ flow/app/controllers/employees_controller.rb
@@ -1,19 +1,15 @@
 class EmployeesController < InheritedResources::Base
 
   def index 
-    # support for searches
-    @employees = Employee.search(params[:search])
+    @employees = Employee.search(params[:search]) # support for searches
   end
 
-  #def new 
-  #  @employee = Employee.new
-  #end
+  def new 
+    @employee = Employee.new
+  end
   
-  # GET /employees/new
-  # GET /employees/new.json
   def new
     @employee = Employee.new
-    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @employee }
@@ -21,4 +17,26 @@ class EmployeesController < InheritedResources::Base
     end
   end
   
+  def create 
+    @employee = Employee.create!(params[:employee])
+    respond_to do |format|
+      format.html { redirect_to employees_url }
+      format.js 
+    end
+  end
+
+  # GET /employees/1
+  # GET /employees/1.json
+  def show
+    @employees = Employee.find(:all) # for add a subordinate
+    
+    @employee = Employee.find(params[:id])
+    
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @employee }
+    end
+  end
+
+
 end
