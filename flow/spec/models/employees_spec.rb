36b8f42e12f8de817e3e2bc3fc6731734604c9e5
Greg Felice <gregfelice@gmail.com>
2013-01-17 16:07:31 -0500

    lots of pretty charts

--- flow/spec/models/employees_spec.rb
+++ flow/spec/models/employees_spec.rb
@@ -5,7 +5,55 @@ require 'yaml'
 
 describe "Employees Model" do 
 
-  it "can retrieve the top level employee", :focus => true do 
+  it "can recurse through the entire tree, starting at the top level boss", :focus => true  do
+
+    employee = create(:employee)
+
+    subordinate_direct_1 = create(:employee, full_name: "Subordinate Direct 1", job_title: "x")
+    subordinate_direct_2 = create(:employee, full_name: "Subordinate Direct 2", job_title: "x")
+    subordinate_indirect_1 = create(:employee, full_name: "Subordinate Indirect 1", job_title: "x")
+    subordinate_indirect_2 = create(:employee, full_name: "Subordinate Indirect 2", job_title: "x")
+    
+    subordinate_direct_1.subordinates << subordinate_indirect_1
+    subordinate_direct_1.subordinates << subordinate_indirect_2
+
+    employee.subordinates << subordinate_direct_1
+    employee.subordinates << subordinate_direct_2
+    
+    # employee.subordinates
+    #final = walk(employee, "", 0)
+    #puts "final: #{final.to_json}"
+
+    final = to_node(employee).to_json
+    puts "final: #{final}"
+    
+  end
+  
+  def to_node(n)
+    {
+      "name" => n.full_name,
+      "children" => n.subordinates.map { |c| to_node c }
+    }
+  end
+
+=begin
+  def walk(node, h, level)
+    ch = {}
+    
+    ch[node.full_name] = node.full_name
+    ch[:children] = []
+
+    node.subordinates.each {|c|
+      ch[:children] << { :name => c.full_name, :size => 1}
+      walk(c, h, level += 1)
+    }
+    puts "level: #{level} node: #{node.full_name} h: #{h}"
+    h.merge ch
+    h
+  end
+=end
+
+  it "can retrieve the top level employee", :focus => false do 
     # employee = Employee.find :all, :conditions => { :supervisors })
     # print employee.inspect
   end
@@ -63,5 +111,8 @@ describe "Employees Model" do
     expect { employee.subordinates << subordinate }.to raise_error(ActiveRecord::RecordInvalid)
 
   end
+
+  it "can not add itself as a supervisor or subordinate", :focus => false
+
   
 end
