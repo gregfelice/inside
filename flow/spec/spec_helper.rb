77b030b04eea0b48deb72f6fba2fb823fe4ae6f6
Greg Felice <gregfelice@gmail.com>
2013-01-23 12:21:06 -0500

    added .rspec file, Guardfile changes, commented out coffee code that is breaking, validations for reporting relationship, column sorting and pagination for employee index, more tests for everything, css changes to support column sorting

--- flow/spec/spec_helper.rb
+++ flow/spec/spec_helper.rb
@@ -1,6 +1,11 @@
+# capybara docs https://gist.github.com/428105
 
 require 'rubygems' # spork
 require 'spork'    # spork
+
+include Warden::Test::Helpers
+Warden.test_mode!
+
 
 ENV["RAILS_ENV"] ||= 'test' # this will automatically require the test section of Gemfile
 
@@ -10,6 +15,7 @@ require 'rspec/autorun'
 
 require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
+# Capybara.default_wait_time = 5
 
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
@@ -20,13 +26,14 @@ Spork.prefork do # spork
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
     config.treat_symbols_as_metadata_keys_with_true_values = true
-    config.filter_run :focus => true
+    #config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
     config.include FactoryGirl::Syntax::Methods
   end
 end
 
 Spork.each_run do
+
   # as per railscasts recommendations
   FactoryGirl.reload # spork related
   
@@ -36,3 +43,50 @@ Spork.each_run do
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 end
 
+
+def login
+  user = nil
+  begin
+    user = User.find_by_email("test_user@test.com")
+  rescue ActiveRecord::RecordNotFound
+    user = create(:user, :email => "test_user@test.com", :password => "p4ssw0rd___")
+  end
+  login_as(user, :scope => :user)
+end
+
+def logout
+  logout(:user)
+end
+
+
+def retry_on_timeout(n = 3, &block)
+  block.call
+rescue Capybara::TimeoutError, Capybara::ElementNotFound => e
+  if n > 0
+    puts "Caught error: #{e.message}. #{n-1} more attempts."
+    retry_on_timeout(n - 1, &block)
+  else
+    raise
+  end
+end
+
+# http://artsy.github.com/blog/2012/02/03/reliably-testing-asynchronous-ui-w-slash-rspec-and-capybara/
+# http://pivotallabs.com/waiting-for-jquery-ajax-calls-to-finish-in-cucumber/
+# brilliant.
+def wait_for_ajax(timeout = Capybara.default_wait_time)
+  page.wait_until(timeout) do
+    page.evaluate_script 'jQuery.active == 0'
+  end
+end
+
+# http://artsy.github.com/blog/2012/02/03/reliably-testing-asynchronous-ui-w-slash-rspec-and-capybara/
+def wait_for_dom(timeout = Capybara.default_wait_time)
+  uuid = SecureRandom.uuid
+  page.find("body")
+  page.evaluate_script <<-EOS
+    _.defer(function() {
+      $('body').append("<div id='#{uuid}'></div>");
+    });
+  EOS
+  page.find("##{uuid}")
+end
