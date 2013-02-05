006aa1b9ebdb2daed6259a6c721d0012880d6f49
Greg Felice <gregfelice@gmail.com>
2013-02-05 12:01:34 -0500

    first shot at unicorn/nginx configs, capistrano; integration of js gantt chart

--- flow/spec/features/authentication_spec.rb
+++ flow/spec/features/authentication_spec.rb
@@ -2,11 +2,12 @@ require 'spec_helper'
 
 feature "Authentication" do 
   
-  scenario "I can log in", :js => true, :focus => false do
+  scenario "I can log in", :js => true, :focus => true do
 
     login
     visit employees_path
     page.should have_content "Logout"
+
   end
 
   scenario "I can log out", :js => true, :focus => false
