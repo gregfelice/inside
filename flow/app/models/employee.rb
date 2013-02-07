5b4c559556306c97b9ccfb2cc0bbda7bcedd874a
Felice <203261@Greg-Felice-8849.local>
2013-02-07 05:16:59 -0500

    fixed bugs with supervisor/subordinate saves, fixed dotted relationship saves, fixed related error handling, added some related tests (more needed), added custom logger code, added thin as development server

--- flow/app/models/employee.rb
+++ flow/app/models/employee.rb
@@ -3,82 +3,140 @@ class Employee < ActiveRecord::Base
   validates_presence_of :full_name, :job_title
   validates_uniqueness_of :full_name
 
+  validates_associated :supervisor_relationships, :subordinate_relationships
+
+  after_validation :update_errors
+
   has_many :supervisor_relationships,  :class_name => "ReportingRelationship", :foreign_key => :subordinate_id, :dependent => :destroy
   has_many :subordinate_relationships, :class_name => "ReportingRelationship", :foreign_key => :supervisor_id,  :dependent => :destroy
 
   has_many :supervisors, :through => :supervisor_relationships
   has_many :subordinates, :through => :subordinate_relationships
 
-  attr_accessible :full_name, :job_title, :level, :cost_center, :contractor, :part_time_status, :supervisor_relationships, :subordinate_relationships, :subordinate_tokens, :supervisor_tokens, :dotted_supervisors, :dotted_supervisor_tokens, :direct_supervisor_tokens
+  attr_accessible :full_name, :job_title, :level, :cost_center, :contractor, :part_time_status, :supervisor_relationships, :subordinate_relationships, :direct_subordinate_tokens, :dotted_subordinate_tokens, :direct_supervisor_tokens, :dotted_supervisor_tokens, :direct_supervisors, :dotted_supervisors, :direct_subordinates, :dotted_subordinates
 
+  # update associated error messages, add to self
+  def update_errors
+    self.supervisor_relationships.reject { |sr| sr.valid? }.each { |sr| sr.errors.messages.each { |key, value| self.errors.add(key, value) } }
+    self.subordinate_relationships.reject { |sr| sr.valid? }.each { |sr| sr.errors.messages.each { |key, value| self.errors.add(key, value) } }
+  end
+
+  # debugging
+  def update_attributes(attributes, options = {})
+    logger.info "employee#update_attributes: #{attributes.inspect}"
+    super(attributes, options)
+  end
+
+  # convenience method, assumes dotted == false
   def add_supervisor(supervisor)
     self.supervisor_relationships.create!( { :dotted => false, :supervisor_id => supervisor.id, :subordinate_id => self.id } )
   end
 
+  # convenience method, assumes dotted == false
   def add_subordinate(subordinate)
     self.subordinate_relationships.create!( { :dotted => false, :supervisor_id => self.id, :subordinate_id => subordinate.id } )
-  end
-
-
-  def dotted_supervisors
-    self.class.dotted_supervisors(self)
   end
 
   def direct_supervisors
     self.class.direct_supervisors(self)
   end
 
+  def dotted_supervisors
+    self.class.dotted_supervisors(self)
+  end
+
+  def direct_subordinates
+    self.class.direct_subordinates(self)
+  end
+
+  def dotted_subordinates
+    self.class.dotted_subordinates(self)
+  end
+
   # jquery-token-input related
-  ###############################################################################################################
-
-  attr_reader :supervisor_tokens
-
-  def supervisor_tokens=(ids) 
-    self.supervisor_ids = ids.split(",").uniq # ensure unique values
-  end
-
-  ###############################################################################################################
-  
-  def dotted_supervisor_tokens
-    Employee.dotted_supervisors(self).map { |ds| ds.id }
-  end
-
-  def dotted_supervisor_tokens=(ids) 
-    dotteds = ids.split(",").flatten.uniq
-    directs = self.direct_supervisor_tokens
-    
-    self.supervisor_relationships.clear
-
-    directs.each { |s| self.supervisor_relationships.build(:dotted => false, :supervisor_id => s, :subordinate_id => self.id) }
-    dotteds.each { |s| self.supervisor_relationships.build(:dotted => true, :supervisor_id => s, :subordinate_id => self.id) }
-
-    self.save!
-  end
-
-  ###############################################################################################################
 
   def direct_supervisor_tokens
     Employee.direct_supervisors(self).map { |ds| ds.id }
   end
-  
-  def direct_supervisor_tokens=(ids) 
+
+  def direct_supervisor_tokens=(ids)
     directs = ids.split(",").flatten.uniq
-    dotteds = self.dotted_supervisor_tokens
-    
+    dotteds = self.supervisor_relationships.reject { |sr| sr.dotted == false }.collect { |sr| sr.supervisor_id }
+
     self.supervisor_relationships.clear
-    
+
     directs.each { |s| self.supervisor_relationships.build(:dotted => false, :supervisor_id => s, :subordinate_id => self.id) }
     dotteds.each { |s| self.supervisor_relationships.build(:dotted => true, :supervisor_id => s, :subordinate_id => self.id) }
 
-    self.save!
+    #logger.info "supervisor relationships after direct sup build: #{self.supervisor_relationships.inspect}"
+
+
+  end
+
+  def dotted_supervisor_tokens
+    Employee.dotted_supervisors(self).map { |ds| ds.id }
+  end
+
+  def dotted_supervisor_tokens=(ids)
+
+    dotteds = ids.split(",").flatten.uniq
+    directs = self.supervisor_relationships.reject { |sr| sr.dotted == true }.collect { |sr| sr.supervisor_id }
+
+    self.supervisor_relationships.clear
+
+    directs.each { |s| self.supervisor_relationships.build(:dotted => false, :supervisor_id => s, :subordinate_id => self.id) }
+    dotteds.each { |s| self.supervisor_relationships.build(:dotted => true, :supervisor_id => s, :subordinate_id => self.id) }
+
   end
 
   ###############################################################################################################
 
-  attr_reader :subordinate_tokens
+  def direct_subordinate_tokens
+    directs = Employee.direct_subordinates(self).map { |ds| ds.id }
+    logger.info "employee#direct_subordinate_tokens: #{directs.inspect}"
+    directs
+  end
 
-  def subordinate_tokens=(ids)
-    self.subordinate_ids = ids.split(",").flatten.uniq # ensure unique values
+  def direct_subordinate_tokens=(ids)
+    directs = ids.split(",").flatten.uniq
+    # dotteds = self.dotted_subordinate_tokens
+    dotteds = self.subordinate_relationships.reject { |sr| sr.dotted == false }.collect { |sr| sr.subordinate_id }
+
+    logger.info "direct subordinate_tokens=() directs: (directs = ids.split(',').flatten.uniq): #{directs.inspect}"
+    logger.info "direct subordinate_tokens=() dotteds: (dotteds = self.dotted_subordinate_tokens): #{dotteds.inspect}"
+
+    self.subordinate_relationships.clear
+
+    directs.each { |s| self.subordinate_relationships.build(:dotted => false, :supervisor_id => self.id, :subordinate_id => s) }
+    dotteds.each { |s| self.subordinate_relationships.build(:dotted => true, :supervisor_id => self.id, :subordinate_id => s) }
+
+    logger.info "subordinate relationships after direct sup build: #{self.subordinate_relationships.inspect}"
+
+  end
+
+  def dotted_subordinate_tokens
+    dotteds = Employee.dotted_subordinates(self).map { |ds| ds.id }
+    logger.info "employee#dotted_subordinate_tokens: #{dotteds.inspect}"
+    dotteds
+  end
+
+  def dotted_subordinate_tokens=(ids)
+
+    dotteds = ids.split(",").flatten.uniq
+    directs = self.subordinate_relationships.reject { |sr| sr.dotted == true }.collect { |sr| sr.subordinate_id }
+
+    logger.info "dotted subordinate_tokens=() directs: (directs = self.direct_subordinate_tokens): #{directs.inspect}"
+    logger.info "dotted subordinate_tokens=() dotteds: (dotteds = ids.split(',').flatten.uniq): #{dotteds.inspect}"
+
+    logger.info "subordinate relationships after dotted sup build, before clear: #{self.subordinate_relationships.inspect}"
+    self.subordinate_relationships.clear
+    logger.info "subordinate relationships after dotted sup build, after clear: #{self.subordinate_relationships.inspect}"
+
+    directs.each { |s| self.subordinate_relationships.build(:dotted => false, :supervisor_id => self.id, :subordinate_id => s) }
+    dotteds.each { |s| self.subordinate_relationships.build(:dotted => true, :supervisor_id => self.id, :subordinate_id => s) }
+
+    logger.info "subordinate relationships after dotted sup build: #{self.subordinate_relationships.inspect}"
+
   end
 
   ###############################################################################################################
@@ -96,7 +154,15 @@ class Employee < ActiveRecord::Base
   scope :dotted_supervisors, lambda { |employee| employee.supervisor_relationships.where(:dotted => true).map { |ds| ds.supervisor } }
 
   # all of my supervisors direct relationship
-  scope :direct_supervisors, lambda { |employee| employee.supervisor_relationships.where("dotted is null OR dotted = false").map { |ds| ds.supervisor } }
+  scope :direct_supervisors, lambda { |employee| employee.supervisor_relationships.where(:dotted => false).map { |ds| ds.supervisor } }
+
+
+  # all of my subordinates with a dotted relationship
+  scope :dotted_subordinates, lambda { |employee| employee.subordinate_relationships.where(:dotted => true).map { |ds| ds.subordinate } }
+
+  # all of my subordinates direct relationship
+  scope :direct_subordinates, lambda { |employee| employee.subordinate_relationships.where(:dotted => false).map { |ds| ds.subordinate } }
+
 
   # give me all employees that are not currently me, or currently my subordinate
   # (not used since show.erb.html changes)
