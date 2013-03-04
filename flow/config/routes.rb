3f70b7aa276f48d1c94ae02cc2ab97fc353b71cb
Greg Felice <gregfelice@gmail.com>
2013-03-04 10:39:39 -0500

    initial commit for people-projects for publish to origin

--- flow/config/routes.rb
+++ flow/config/routes.rb
@@ -1,5 +1,12 @@
 Flow::Application.routes.draw do
 
+  resources :portfolios
+  resources :plans
+  resources :milestones
+  resources :resource_allocations
+  resources :people
+  resources :person_associations
+
   devise_for :users
 
   resources :widgets do
@@ -11,78 +18,17 @@ Flow::Application.routes.draw do
       match 'search' => 'employees#search', :via => [:get, :post], :as => :search
     end
   end
-
   resources :reporting_relationships
 
   get "home/index"
 
   match "/release_calendar"          => "home#release_calendar"
-
   match "/orgchart"                  => "home#orgchart"
-
   match "/orgdendro"                 => "home#orgdendro"
   match "/orgdendro_tree"            => "home#orgdendro_tree" # json
-
   match "/budgetchart"               => "home#budgetchart"
   match "/budgetchart_dynamic"       => "home#budgetchart_dynamic"
-
   match "/staffingchart"             => "home#staffingchart"
 
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'home#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
-
 end
--- flow/spec/factories/factories.rb
+++ flow/spec/factories/factories.rb
@@ -4,7 +4,36 @@ FactoryGirl.define do
 
   factory :employee do
     sequence(:full_name) {|n| "Joe #{n}" }
-    #full_name "employee"
+    # full_name "employee"
     job_title "president"
   end
+
+  factory :person do
+    sequence(:name) {|n| "Joe #{n}" }
+    title "worker"
+    person_type "employee"
+  end
+
+  factory :portfolio do
+    sequence(:name) {|n| "Portfolio #{n}" }
+    description "this is a portfolio"
+  end
+
+  factory :plan do
+    sequence(:name) {|n| "Plan #{n}" }
+    description "this is a plan"
+    portfolio_category "mobile"
+  end
+
+  factory :milestone do
+    sequence(:name) {|n| "Milestone #{n}" }
+    business_driver "user experience"
+    description "this is a milestone."
+    health "green"
+    planned_start Date.today
+    planned_finish Date.today.next_month
+    status "started"
+  end
+
+
 end
