158a9e8486ad8a6312e8048b2acb26e1bd46da3c
Greg Felice <gregfelice@gmail.com>
2013-03-18 11:39:48 -0400

    various work on person and person assocations
    fixed symbols vs strings for person and person association type checks
    updated model and interactive tests
    added new field validations

--- flow/app/models/person_association.rb
+++ flow/app/models/person_association.rb
@@ -1,5 +1,11 @@
 class PersonAssociation < ActiveRecord::Base
 
+  @association_types = ['direct_reporting', 'dotted_reporting']
+
+  class << self
+    attr_accessor :association_types
+  end
+
   attr_accessible :description, :sink_id, :source_id, :association_type, :source, :sink
 
   belongs_to :source,   :class_name => "Person"
@@ -8,6 +14,6 @@ class PersonAssociation < ActiveRecord::Base
   validates_presence_of :association_type
   validates_uniqueness_of :sink_id, :scope => [:source_id, :association_type]
 
-  validates :association_type, :inclusion => { :in => [:direct_reporting, :dotted_reporting], :message => "%{value} is not a valid person association type" }
+  validates :association_type, :inclusion => { :in => @association_types, :message => "%{value} is not a valid person association type" }
 
 end
