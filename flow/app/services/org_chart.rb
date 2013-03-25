87ac22b44e1cd041c9be80bab849fe61427628a5
Greg Felice <gregfelice@gmail.com>
2013-03-25 15:45:45 -0400

    commit of a basic orgchart that can be navigated (graphviz)
    dynamic construction of graphviz dot chart
    dynamic URLs for navigating structure and showing people details
    some more complex and experimental layout edges (commented out for now)

--- flow/app/services/org_chart.rb
+++ flow/app/services/org_chart.rb
@@ -13,11 +13,11 @@ class OrgChart
   def generate_org_context_svg_xml(person_id, max_depth)
 
     start = Time.now.to_f
-    g = GraphViz.digraph( :G, :splines => :curved, :ratio => :auto, :size => "11,8" )
+    g = GraphViz.digraph( :G, :splines => :curved, :ratio => :auto, :concentrate => true) #, :size => "10.5,8" )
 
     #g = GraphViz.digraph( :G, :splines => :curved, :use => "fdp")
     #g = GraphViz.new( :G, :type => :digraph, :splines => :ortho, :layout => :dot )
-    #g[:rankdir] = "LR"
+    g[:rankdir] = "LR"
 
     # set global node options
     g.node[:color]    = "#ddaa66"
@@ -27,7 +27,7 @@ class OrgChart
     g.node[:penwidth] = "1"
     g.node[:fontname] = "Arial"
     #g.node[:fontname] = "Trebuchet MS"
-    g.node[:fontsize] = "7"
+    g.node[:fontsize] = "10"
     g.node[:fillcolor]= "#ffeecc"
     g.node[:fontcolor]= "#775500"
     g.node[:margin]   = "0.1"
@@ -45,33 +45,22 @@ class OrgChart
 
     g.add_nodes(p.id.to_s, :label => p.name).fillcolor("darkseagreen1")
 
-    current_depth = 0
+    max_depth = 20
+    rec_sources(g, p, max_depth)
+    rec_sinks(g, p, max_depth)
 
-    # TODO rec_sources(g, p, 5)
-    rec_sinks(g, p, 3)
+    Rails.logger.info g.inspect
 
     svg_xml = g.output( :svg => String )
+    # svg_xml = g.output( :png => "foo.png" )
     Rails.logger.info "Time to generate org chart: #{Time.now.to_f - start}"
 
     svg_xml
   end
 
   private
-=begin
-  def rec_sources(g, node, max_depth, current_depth=0, nodes={})
-    current_depth = current_depth + 1
-    return if current_depth == max_depth || !nodes[node.name].nil?
-    node.source_associations.each {|sa|
-      style = get_style(sa.association_type)
 
-      g.add_edges(sa.source.name, node.name).style(style)
-      rec_sources(g, sa.source, max_depth, current_depth, nodes)
-    }
-    nodes[node.name] = node.name
-  end
-=end
-
-  def rec_sources(g, subject, max_depth, current_depth=0, nodes={})
+  def old_rec_sources(g, subject, max_depth, current_depth=0, nodes={})
     id = subject.id.to_s
     current_depth = current_depth + 1
     return if current_depth == max_depth || !nodes[id].nil?
@@ -79,57 +68,178 @@ class OrgChart
     subject.source_associations.each {|sa|
       sa_id = sa.id.to_s
       nodes[sa_id] = g.add_nodes(sa_id, 'label' => sa.source.name)
-      g.add_edges(nodes[sa_id], nodes[id]).style(get_style(sa.association_type))
+      g.add_edges(nodes[sa_id], nodes[id]).style(get_edge_style(sa.association_type))
       rec_sources(g, sa.source, max_depth, current_depth, nodes)
     }
   end
 
+  # http://techslides.com/over-1000-d3-js-examples-and-demos/
+  # http://bl.ocks.org/mbostock/1138500
+  # http://bl.ocks.org/mbostock/3311124
+  # http://bl.ocks.org/mbostock/4063570
 
-#http://techslides.com/over-1000-d3-js-examples-and-demos/
-#http://bl.ocks.org/mbostock/1138500
-# http://bl.ocks.org/mbostock/3311124
-#http://bl.ocks.org/mbostock/4063570
 
-  # note: we are doing this with a db recursion. expensive...
-  def rec_sinks(g, subject, max_depth, current_depth=0, nodes={}, edges={})
+  def get_node_color(person)
+    return :lightgray if person.hiring_status == 'open'
+    return :wheat if person.person_type == 'contractor'
+    return :snow
+  end
+
+  def rec_sources(g, subject, max_depth, current_depth=0, nodes={}, edges={}, subgraphs={})
+    current_depth = current_depth + 1
+    id = subject.id.to_s
+    return if current_depth == max_depth
+    (nodes[id] = g.add_nodes(id,
+        'label' => subject.name,
+        :URL => Rails.application.routes.url_helpers.person_path(:id => id),
+        :target => "_parent"
+        )) if !nodes.has_key?(id)
+    subject.source_associations.each {|sa|
+      sa_id = sa.source.id.to_s
+      next if edges.has_key?([ nodes[sa_id], nodes[id] ])
+      nodes[sa_id] = g.add_nodes(
+        sa_id,
+        :label => sa.source.name + "\\n" + sa.source.title,
+        :fillcolor => get_node_color(sa.source),
+        :URL => Rails.application.routes.url_helpers.person_path(:id => sa_id),
+        :target => "_parent"
+        ) unless nodes.has_key?(sa_id)
+    }
+    subject.source_associations.each {|sa|
+      sa_id = sa.source.id.to_s
+      next if edges.has_key?([ nodes[sa_id], nodes[id] ])
+      edges[ [ nodes[sa_id], nodes[id] ] ] = g.add_edges(
+        nodes[sa_id], nodes[id],
+        :style => get_edge_style(sa.association_type),
+        :label => current_depth.to_s,
+        :edgeURL => Rails.application.routes.url_helpers.chart_org_context_path(:id => sa_id),
+        :target => "_parent"
+        )
+      rec_sources(g, sa.source, max_depth, current_depth, nodes, edges, subgraphs)
+    }
+  end
+
+
+  def rec_sinks(g, subject, max_depth, current_depth=0, nodes={}, edges={}, subgraphs={})
+    current_depth = current_depth + 1
+    id = subject.id.to_s
+    return if current_depth == max_depth
+    (nodes[id] = g.add_nodes(id,
+        'label' => subject.name,
+        :URL => Rails.application.routes.url_helpers.person_path(:id => id),
+        :target => "_parent"
+        )) if !nodes.has_key?(id)
+    subject.sink_associations.each {|sa|
+      sa_id = sa.sink.id.to_s
+      next if edges.has_key?([ nodes[id], nodes[sa_id] ])
+      nodes[sa_id] = g.add_nodes(
+        sa_id,
+        :label => sa.sink.name + "\\n" + sa.sink.title,
+        :fillcolor => get_node_color(sa.sink),
+        :URL => Rails.application.routes.url_helpers.person_path(:id => sa_id),
+        :target => "_parent"
+        ) unless nodes.has_key?(sa_id)
+    }
+    subject.sink_associations.each {|sa|
+      sa_id = sa.sink.id.to_s
+      next if edges.has_key?([ nodes[id], nodes[sa_id] ])
+      edges[ [ nodes[id], nodes[sa_id] ] ] = g.add_edges(
+        nodes[id], nodes[sa_id],
+        :style => get_edge_style(sa.association_type),
+        :label => current_depth.to_s,
+        :edgeURL => Rails.application.routes.url_helpers.chart_org_context_path(:id => sa_id),
+        :target => "_parent"
+        )
+      rec_sinks(g, sa.sink, max_depth, current_depth, nodes, edges, subgraphs)
+    }
+  end
+
+
+  def complex_rec_sinks(g, subject, max_depth, current_depth=0, nodes={}, edges={}, subgraphs={})
     current_depth = current_depth + 1
     id = subject.id.to_s
     return if current_depth == max_depth
     (nodes[id] = g.add_nodes(id, 'label' => subject.name)) if !nodes.has_key?(id)
 
+    # subgraphs
+    if current_depth == 2 && false
+      subgraphs[id] = g.add_graph(
+        "cluster_#{id}",
+        :fontname => "Arial",
+        :color => :lightgrey,
+        #:label => "#{subject.name} Area"
+        #:bgcolor => "orange",
+        ) unless subgraphs.has_key?("cluster_#{id}")
+      Rails.logger.info "subgraphs: #{subgraphs.size} added for key: #{id} : #{subject.name}"
+      g = subgraphs[id]
+      new_subgraph = true
+    else
+      new_subgraph = false
+    end
 
+    # real nodes
     subject.sink_associations.each {|sa|
       sa_id = sa.sink.id.to_s
       next if edges.has_key?([ nodes[id], nodes[sa_id] ])
 
       nodes[sa_id] = g.add_nodes(
         sa_id,
-        :group => id,
         :label => sa.sink.name + "\\n" + sa.sink.title,
-        :fillcolor => sa.sink.person_type == 'contractor' ? "wheat" : "snow") unless nodes.has_key?(sa_id)
+        :fillcolor => get_node_color(sa.sink.person_type),
+        #:URL => Rails.application.routes.url_helpers.person_path(sa_id)
+        :URL => Rails.application.routes.url_helpers.chart_org_context_path(:id => sa_id),
+        :target => "_parent"
+        ) unless nodes.has_key?(sa_id)
+    }
 
-      #if current_depth < 5
-      #  g.add_edges(id, sa_id,
-      #    :style => :invisible
-      #    )
-      #end
+    # layout edges
+    sink_assocs = subject.sink_associations
+    i = 0
+    for i in 0..sink_assocs.size
+      if !sink_assocs[i].nil? && !sink_assocs[i+1].nil? && current_depth > 1 && false
+        node_a_id = sink_assocs[i].sink.id.to_s
+        node_b_id = sink_assocs[i+1].sink.id.to_s
+        g.add_edges(node_a_id, node_b_id,
+          :style => :solid,
+          :color => "orchid",
+          #:style => :invis,
+          #:weight => 10
+          )
+      else
+        break
+      end
+    end
 
+    # real edges, and recursion
+    # ltail for these people should point to the g.id
+    subject.sink_associations.each {|sa|
+      sa_id = sa.sink.id.to_s
+      next if edges.has_key?([ nodes[id], nodes[sa_id] ])
 
-      edges[ [ nodes[id], nodes[sa_id] ] ] = g.add_edges(
-        nodes[id], nodes[sa_id],
-        :style => get_style(sa.association_type),
-        :label => current_depth.to_s
-        )
-
-      rec_sinks(g, sa.sink, max_depth, current_depth, nodes, edges)
+      if true # current_depth < 2
+        edges[ [ nodes[id], nodes[sa_id] ] ] = g.add_edges(
+          nodes[id], nodes[sa_id],
+          :style => get_edge_style(sa.association_type),
+          :label => current_depth.to_s,
+          :lhead => nodes[sa_id],
+          :ltail => g.id,
+          #:weight => -10,
+          :nojustify => false,
+          :len => 0.3,
+          #:style => :invis
+          )
+      end
+      rec_sinks(g, sa.sink, max_depth, current_depth, nodes, edges, subgraphs)
     }
   end
+
+
 
   def line_breaks(s)
     s.scan(/.*b{1}/).join("\n")
   end
 
-  def get_style(association_type)
+  def get_edge_style(association_type)
     case association_type
     when 'direct_reporting'
       style = :solid
