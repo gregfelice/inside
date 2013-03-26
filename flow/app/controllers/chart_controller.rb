b8e51035f4af15edd3fbad9d497bf20b42a73c1a
Greg Felice <gregfelice@gmail.com>
2013-03-26 11:23:35 -0400

    stable org chart version allowing naviation from node to node

--- flow/app/controllers/chart_controller.rb
+++ flow/app/controllers/chart_controller.rb
@@ -1,11 +1,24 @@
 class ChartController < ApplicationController
 
+=begin
+
+show / dont show titles
+
+show 2 levels, 3 levels... what about more / less levels... yes.. i like that.
+
+start with 2.
+
+ok - so, that's a button and a saved value..
+
+=end
+
   def org_context
-    person_id = params[:id]                  #2942 # marc for starters
-    @person = Person.find(person_id)
-    logger.info "PERSONID #{person_id}"
-    max_depth = 2
-    @svg_xml = OrgChart.instance.generate_org_context_svg_xml(person_id, max_depth)
+
+    max_sink_depth = params[:max_sink_depth].nil? ? 2 : params[:max_sink_depth]
+    @svg_xml = OrgChart.instance.generate_org_context_svg_xml(params[:id], max_sink_depth.to_i)
+    @person = Person.find(params[:id])
+    @max_sink_depth = params[:max_sink_depth]
+
     respond_to do |format|
       format.html { render :layout => 'full_screen' }
       format.svg { render :xml => @svg_xml }
